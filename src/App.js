7b01a5d2cb707ff0acdfb1dd84974c130a6a8e1a
Randy <randyisbusy@gmail.com>
2020-07-03 10:04:34 -0700

    upated back end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{  
+  componentDidMount(){
+    fetch('http://localhost:3000/patients') 
+    .then(res => res.json()) 
+    .then(data => console.log(data)) 
+    .catch(err =>console.log(err))
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>YO YO YO</h1>
+        <p>  
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -22,5 +30,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
