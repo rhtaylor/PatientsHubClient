a1d9c836077521980d39b4cde29967004db9c086
Randy <randyisbusy@gmail.com>
2020-07-03 10:59:15 -0700

    updated front-end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'; 
 import logo from './logo.svg';
 import './App.css';
-
+import Provider from './components/Provider'
 class App extends Component{  
   componentDidMount(){
     fetch('http://localhost:3000/patients') 
@@ -18,14 +18,9 @@ class App extends Component{
         <p>  
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+         
+       <Provider />
       </header>
     </div>
   );
---
+++ src/components/Provider.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Provider extends Component{
+    render(){
+        return(<div>
+            <h1>Provider Hub</h1>
+        </div>)
+
+
+    }
+}
